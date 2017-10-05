24c682ca4b932e62ad31abc039524417e6de9025
eric <hw1208.ahn@gmail.com>
2017-10-05 21:42:55 +0900

    static added

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-//index.js
+// index.js
+﻿
+﻿var express = require('express');
+var app = express();
 
-﻿var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
-var app = express(); //express를 실행하여 app object를 초기화 합니다.
+app.use(express.static(__dirname + '/public')); // 1
 
-app.get('/',function (req,res) { // '/' 위치에 'get'요청을 받는 경우,
- res.send('Hello World!'); // "Hello World!"를 보냅니다.
-});
-
-app.listen(3000, function(){ //3000번 포트를 사용합니다.
- console.log('Server On!'); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
+app.listen(3000, function(){
+ console.log('Server On!');
 });
