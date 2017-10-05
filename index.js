fc6e9688a4ce1febbdc31c64393267d7bd644504
eric <hw1208.ahn@gmail.com>
2017-10-05 21:52:11 +0900

    dynamic added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // index.js
-﻿
+
 ﻿var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public')); // 1
+app.set("view engine","ejs"); // 1
+app.use(express.static(__dirname + '/public'));
+
+// 2
+app.get("/hola", function(req,res){
+ res.render("hola", {name:req.query.nameQuery});
+});
+
+// 3
+app.get("/hola/:nameParam", function(req,res){
+ res.render("hola", {name:req.params.nameParam});
+});
 
 app.listen(3000, function(){
  console.log('Server On!');
